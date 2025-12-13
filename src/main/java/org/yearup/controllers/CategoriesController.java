2a3591f8f67d52598ff971a8b0ba654f69ac18cc
Nachi4265 <hvirgen@my.yearupunited.org>
2025-12-12 23:37:10 -0500

    First test and commit

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,6 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -10,10 +8,13 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+
+
+
+@RestController// add the annotations to make this a REST controller
+@RequestMapping("categories")// add the annotation to make this controller the endpoint for the following url
+// http://localhost:8080/categories
+@CrossOrigin// add annotation to allow cross site origin requests
 public class CategoriesController
 {
     private CategoryDao categoryDao;
